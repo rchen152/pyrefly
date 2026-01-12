04e7c76a56cfb48b9cccbc7a16e862ac2016407d
Steven Troxler <stroxler@meta.com>
2026-01-12 10:10:02 -0800

    Add license to website/src/theme/prism-include-languages.ts
    Summary: Add missing MIT license header to prism-include-languages.ts file.
    
    Reviewed By: migeed-z
    
    Differential Revision: D90511693
    
    fbshipit-source-id: 2b0b219232b1f1abac4fdd661b97a50992828d3c

--- website/src/theme/prism-include-languages.ts
+++ website/src/theme/prism-include-languages.ts
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
 import siteConfig from '@generated/docusaurus.config';
 import type * as PrismNamespace from 'prismjs';
 import type { Optional } from 'utility-types';
