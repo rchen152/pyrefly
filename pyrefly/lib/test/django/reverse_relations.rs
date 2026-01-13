c7a983b1c8345df636860226596127c9fa29913e
Zeina Migeed <migeedz@meta.com>
2026-01-12 23:05:47 -0800

    Add failing unittest for custom reverse relations (related_name keyword) (#2087)
    Summary:
    Pull Request resolved: https://github.com/facebook/pyrefly/pull/2087
    
    Here, we test the related_name keyword which allows us to customize the reverse relation
    
    Reviewed By: aleivag
    
    Differential Revision: D90517278
    
    fbshipit-source-id: f60dc4fe66a185c395a50cb1bbea384e86efc57e

--- pyrefly/lib/test/django/reverse_relations.rs
+++ pyrefly/lib/test/django/reverse_relations.rs
@@ -24,3 +24,21 @@ reporter = Reporter()
 reporter.article_set  # E: `Reporter` has no attribute `article_set`
 "#,
 );
+
+django_testcase!(
+    bug = "Reverse relations not yet implemented",
+    test_foreign_key_reverse_custom_name,
+    r#"
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='written_books')
+
+author = Author()
+# Custom related_name should be used instead of default
+author.written_books  # E: `Author` has no attribute `written_books`
+"#,
+);
