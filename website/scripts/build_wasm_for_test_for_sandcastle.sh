ccc90cdb112f93bb9b2e91510c0ce07fe873353c
Steven Troxler <stroxler@meta.com>
2026-01-13 08:14:35 -0800

    Explain the wasm test setup better
    Summary:
    I found the comment frustrating, it expects me to think and disambiguate
    the vague hint at what to do; it's much clearer to put a little scriptlet
    in the comment making it very clear what to do.
    
    Note that in spite of the name, the referenced build helper script is
    open-source. It does a bit more than strictly necessary if someone already has
    cargo wasm set up then the script does a bit more than needed, but I think
    it's good to use it anyway since often a contributor (including someone on
    our team) won't have done this before or won't have done it recently.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D90539384
    
    fbshipit-source-id: 3c528449385ca94b9f49a4bc9422e303cc35f81e

--- website/scripts/build_wasm_for_test_for_sandcastle.sh
+++ website/scripts/build_wasm_for_test_for_sandcastle.sh
@@ -4,7 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-# This script is used by Sandcastle to build the WASM for the test page.
+# This script is used by Sandcastle (Meta's internal CI) to build the WASM for
+# the test page, and it can also be useful for testing changes to the website
+# code as an open-source developer.
 
 # Fail if we have any errors
 set -e
--- website/src/__tests__/wasm/pyrefly_wasm_for_testing.js
+++ website/src/__tests__/wasm/pyrefly_wasm_for_testing.js
@@ -8,5 +8,15 @@
  */
 
 // This is a stub to ensure that we can still resolve modules in website tests.
-// Currently, you need to build the wasm and js binding and copy them over here to replace this file.
+//
+// Currently, you need to build the wasm and js binding and copy them over here to replace this file. We
+// have a bash script to do this that should work; when you finish you need to revert the changes.
+// ```
+// cd pyrefly/website
+// cp src/__tests__/wasm/pyrefly_wasm_for_testing.js src/__tests__/wasm/pyrefly_wasm_for_testing.js.bak
+// bash scripts/build_wasm_for_test_for_sandcastle.sh
+// yarn test
+// mv src/__tests__/wasm/pyrefly_wasm_for_testing.js.bak src/__tests__/wasm/pyrefly_wasm_for_testing.js
+// ```
+// When you finish testing, you'll need to revert changes to this file before you commit.
 module.exports = {};
