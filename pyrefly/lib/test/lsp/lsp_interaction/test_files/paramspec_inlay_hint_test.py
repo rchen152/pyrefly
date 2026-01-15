afc0ecb6897afb7c5c8cd0f6eaa7e25236d2af64
John Van Schultz <jvansch@meta.com>
2026-01-15 11:19:04 -0800

    Try to fix ParamSpec test for windows. (#2121)
    Summary:
    Pull Request resolved: https://github.com/facebook/pyrefly/pull/2121
    
    Up until 3.10 typing_extensions defined ParamSpec locally. Starting in 3.10 and greater typing extensions instead re-exports ParamSpec from typing.
    
    I believe this may have been the issue causing this test to fail. All the flaky tests we have seen on Windows have been features that have been gated by specific versions.
    
    In order to account for this, we can always trying getting ParamSpec from typing_extensions since it will always be available there.
    
    Reviewed By: kinto0
    
    Differential Revision: D90777432
    
    fbshipit-source-id: 7a2b158b4ac86e923ed4401480287b61011e06f9

--- pyrefly/lib/test/lsp/lsp_interaction/test_files/paramspec_inlay_hint_test.py
+++ pyrefly/lib/test/lsp/lsp_interaction/test_files/paramspec_inlay_hint_test.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import ParamSpec
+from typing_extensions import ParamSpec
 
 P = ParamSpec("P")
 
