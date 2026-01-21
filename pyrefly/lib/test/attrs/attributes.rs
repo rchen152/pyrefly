8ddc26c129e5e3bdde79d3e77a1aa2b45766a2b9
Zeina Migeed <migeedz@meta.com>
2026-01-21 10:45:16 -0800

    Add testcase for missing attr handling
    Summary:
    Internal user has reported this issue here:
    https://fb.workplace.com/groups/1355816225674448/permalink/1530835144839221/
    
    opened issue https://github.com/facebook/pyrefly/issues/2177
    
    Reviewed By: aleivag, ndmitchell
    
    Differential Revision: D91090389
    
    fbshipit-source-id: fab042259a1bdc17a03427f9c933103e9b478efc

--- pyrefly/lib/test/attrs/attributes.rs
+++ pyrefly/lib/test/attrs/attributes.rs
@@ -22,3 +22,29 @@ class Example:
 obj = Example(private="secret", public=42) # E: Missing argument `_private` in function `Example.__init__` # E: Unexpected keyword argument `private` in function `Example.__init__` 
 "#,
 );
+
+attrs_testcase!(
+    bug = "fix attr handling as shown in comments below ",
+    test_attrs_basic,
+    r#"
+from typing import reveal_type
+
+import attr
+
+@attr.s()
+class A:
+    x: int | None = attr.ib(None)
+
+@attr.s()
+class B:
+    x: int | None = None
+
+# this should not be an error
+A()  # E: Missing argument `x` in function `A.__init__`  
+B() 
+
+A("hello")  # should be an error
+B("hello") # E: Argument `Literal['hello']` is not assignable to parameter `x` with type `int | None` in function `B.__init__`
+
+"#,
+);
