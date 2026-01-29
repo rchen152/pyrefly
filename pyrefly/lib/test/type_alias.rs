ddd78961762300a27f4663dd0ed236f098eb4ff0
Zeina Migeed <migeedz@meta.com>
2026-01-28 18:31:29 -0800

    Add failing variance testcase with type aliases
    Reviewed By: grievejia
    
    Differential Revision: D91724723
    
    fbshipit-source-id: 6f2c9345401385882ec268091c8384f1d6402488

--- pyrefly/lib/test/type_alias.rs
+++ pyrefly/lib/test/type_alias.rs
@@ -1048,3 +1048,30 @@ def bad_type_aliases(
     pass
 "#,
 );
+
+testcase!(
+    bug = "conformance: Should error on variance incompatibility in type alias class declarations",
+    test_type_alias_variance_conformance,
+    r#"
+from typing import Generic, TypeVar, TypeAlias
+
+T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
+T_contra = TypeVar("T_contra", contravariant=True)
+
+class ClassA(Generic[T]): ...
+
+A_Alias_1: TypeAlias = ClassA[T_co]
+A_Alias_2: TypeAlias = A_Alias_1[T_co]
+
+class ClassA_1(ClassA[T_co]): ...  # should error: incompatible variance
+class ClassA_2(A_Alias_1[T_co]): ...  # should error: incompatible variance
+class ClassA_3(A_Alias_2[T_co]): ...  # should error: incompatible variance
+
+class ClassB(Generic[T, T_co]): ...
+
+B_Alias_1 = ClassB[T_co, T_contra]
+
+class ClassB_1(B_Alias_1[T_contra, T_co]): ...  # should error: incompatible variance
+"#,
+);
